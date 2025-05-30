69b7edb4813643fb286e5eea92e26d96ab716084
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-05-30 13:48:11 +0300

    Initial commit

--- src/interpreters/brainfuck.c
+++ src/interpreters/brainfuck.c
@@ -2,3 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 #include "brainfuck.h"
+
+
+typedef enum{
+    OP_INC_PC = '>',
+    OP_DEC_PC = '<',
+    OP_INC_VAL = '+',
+    OP_DEC_VAL = '-',
+    OP_IN = ',',
+    OP_OUT = '.',
+    OP_JMP_FWD = '[',
+    OP_JMP_BCK = ']'
+}OP;
+
+int execute_bf_file(FILE *ptr){
+    return EXIT_SUCCESS;
+}
+
+int execute_bf(char *str){
+    FILE *fptr;
+
+    if((fptr = fopen(str, "r")) == NULL){
+        fprintf(stderr, "Failure opening input file: %s\n", str);
+        return EXIT_FAILURE;
+    }
+
+    int status = execute_bf_file(fptr);
+
+    fclose(fptr);
+
+    return status;
+}
