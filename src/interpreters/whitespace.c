33e3aabc15880ae7946868c8f364f3fb845bcab0
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-06-01 13:05:05 +0300

    Set up IMP execution pipeline

--- src/interpreters/whitespace.c
+++ src/interpreters/whitespace.c
@@ -2,7 +2,57 @@
 #include <stdlib.h>
 #include "whitespace.h"
 
+typedef struct{
+    size_t Spaces, Tabs, instruction_index;
+}Label;
+
 int execute_whitespace_file(FILE* fptr){
+    int c1, c2;
+
+    size_t stack_cap = 20, heap_cap = 10, labels_cap = 5;
+    size_t *stack = calloc(stack_cap, sizeof(size_t));
+    if(!stack){
+        fprintf(stderr, "Failure allocating memory\n");
+        return EXIT_FAILURE;
+    }
+    size_t *heap = calloc(heap_cap, sizeof(size_t));
+    if(!heap){
+        fprintf(stderr, "Failure allocating memory\n");
+        return EXIT_FAILURE;
+    }
+    Label *labels = calloc(labels_cap, sizeof(Label));
+    if(!labels){
+        fprintf(stderr, "Failure allocating memory\n");
+        return EXIT_FAILURE;
+    }
+
+    while((c1 = fgetc(fptr)) != EOF){
+        switch(c1){
+            case LF:
+                //handle_flow_control(fptr, stack, labels);
+                break;
+            case SPACE:
+                //handle_stack_manip(stack);
+                break;
+            case TAB:
+                c2 = fgetc(fptr);
+                switch (c2){
+                    case LF:
+                        //handle_io(stack, heap);
+                        break;
+                    case SPACE:
+                        //handle_arithmetic(stack);
+                        break;
+                    case TAB:
+                        //logic
+                        break;
+                    
+                    default:
+                        return EXIT_FAILURE;
+                }
+        }
+    }
+
     return EXIT_SUCCESS;
 }
 
@@ -19,4 +69,20 @@ int execute_whitespace(char *file_name){
     fclose(fptr);
 
     return status;
+}
+
+void handle_flow_control(FILE *fptr, size_t *stack, Label *labels){
+
+}
+
+void handle_stack_manip(size_t *stack){
+
+}
+
+void handle_io(size_t *stack, size_t *heap){
+
+}
+
+void handle_arithmetic(size_t *stack){
+
 }
--- src/interpreters/whitespace.h
+++ src/interpreters/whitespace.h
@@ -1,1 +1,13 @@
+#define SPACE 32
+#define TAB 9
+#define LF 10
+
 int execute_whitespace(char *);
+
+void handle_flow_control(FILE *, size_t *, Label *);
+
+void handle_stack_manip(size_t *);
+
+void handle_io(size_t *, size_t *);
+
+void handle_arithmetic(size_t *);
