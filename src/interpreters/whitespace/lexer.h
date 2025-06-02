19a94794e930e58ef2d96dcabb59b23e7ff59614
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-06-02 15:26:48 +0300

    Fix hex values

--- src/interpreters/whitespace/lexer.h
+++ src/interpreters/whitespace/lexer.h
@@ -5,34 +5,34 @@
 
 #define TOKENS_CAP 20
 
-#define IO_TS 0x0
-#define IO_TT 0x1
-#define IO_SS 0x2
-#define IO_ST 0x3
+#define IO_TS 0x00
+#define IO_TT 0x01
+#define IO_SS 0x02
+#define IO_ST 0x03
 
-#define SM_S_n 1x0
-#define SM_LS 1x1
-#define SM_LT 1x2
-#define SM_LL 1x3
-#define SM_TS_n 1x4
-#define SM_TL_n 1x5
+#define SM_S_n 0x10
+#define SM_LS 0x11
+#define SM_LT 0x12
+#define SM_LL 0x13
+#define SM_TS_n 0x14
+#define SM_TL_n 0x15
 
-#define A_SS 2x0
-#define A_ST 2x1
-#define A_SL 2x2
-#define A_TS 2x3
-#define A_TT 2x4
+#define A_SS 0x20
+#define A_ST 0x21
+#define A_SL 0x22
+#define A_TS 0x23
+#define A_TT 0x24
 
-#define FC_SS_l 3x0
-#define FC_ST_l 3x1
-#define FC_Sl_l 3x2
-#define FC_TS_l 3x3
-#define FC_TT_l 3x4
-#define FC_TL 3x5
-#define FC_LL 3x6
+#define FC_SS_l 0x30
+#define FC_ST_l 0x31
+#define FC_Sl_l 0x32
+#define FC_TS_l 0x33
+#define FC_TT_l 0x34
+#define FC_TL 0x35
+#define FC_LL 0x36
 
-#define HP_S 4x0
-#define HP_T 4x1
+#define HP_S 0x40
+#define HP_T 0x41
 
 
 char *tokenize_whitespace(FILE *);
