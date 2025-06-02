851c6857f6aa77efb0d605f99d30ade3dda79d9a
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-06-02 16:17:34 +0300

    Feat Stack Manipulation tokenization

--- src/interpreters/whitespace/lexer.c
+++ src/interpreters/whitespace/lexer.c
@@ -112,6 +112,60 @@ int tokenize_heap(FILE *fptr, char **tokens, size_t *tokens_cap, size_t *tokens_
     return EXIT_SUCCESS;
 }
 
+int tokenize_stack_manip(FILE *fptr, char **tokens, size_t *tokens_cap, size_t *tokens_count){
+    int c1 = fgetc(fptr);
+
+    if(*tokens_cap == (*tokens_count)){
+        if(ensure_cap(tokens, tokens_cap, tokens_count) == EXIT_FAILURE)
+            return EXIT_FAILURE;
+    }
+
+    switch(c1){
+        case SPACE:
+            (*tokens)[(*tokens_count)++] = SM_S_n;
+            return EXIT_SUCCESS;
+        case LF:
+            int c2_lf = fgetc(fptr);
+            int key = (c1 << 8) | c2_lf;
+            switch(key){
+                case (LF << 8) | SPACE:
+                    (*tokens)[(*tokens_count)++] = SM_LS;
+                    return EXIT_SUCCESS;
+
+                case (LF << 8) | TAB:
+                    (*tokens)[(*tokens_count)++] = SM_LT;
+                    return EXIT_SUCCESS;
+
+                case (LF << 8) | LF:
+                    (*tokens)[(*tokens_count)++] = SM_LL;
+                    return EXIT_SUCCESS;
+
+                default:
+                    fprintf(stderr, "Unrecognised character sequence while tokenizing whitespace Stack Manipulation command: %c %c (ASCII: %d %d)", c1, c2_lf, c1, c2_lf);
+                    return EXIT_FAILURE;
+            }
+        case TAB:
+            int c2_tab = fgetc(fptr);
+            int key = (c1 << 8) | c2_tab;
+            switch(key){
+                case (TAB << 8) | SPACE:
+                    (*tokens)[(*tokens_count)++] = SM_TS_n;
+                    return EXIT_SUCCESS;
+
+                case (TAB << 8) | LF:
+                    (*tokens)[(*tokens_count)++] = SM_TL_n;
+                    return EXIT_SUCCESS;
+
+                default:
+                    fprintf(stderr, "Unrecognised character sequence while tokenizing whitespace Stack Manipulation command: %c %c (ASCII: %d %d)", c1, c2_tab, c1, c2_tab);
+                    return EXIT_FAILURE;
+            }
+        default:
+            fprintf(stderr, "Unrecognised character while tokenizing whitespace Stack Manipulation command: %c (ASCII: %d)", c1, c1);
+            return EXIT_FAILURE;
+    }
+}
+
 char *tokenize_whitespace(FILE *fptr){
     size_t tokens_cap = TOKENS_CAP, tokens_count = 0;
     char *tokens = calloc(tokens_cap, sizeof(char));
@@ -140,6 +194,10 @@ char *tokenize_whitespace(FILE *fptr){
                         break;
                     default:
                         fprintf(stderr, "Unrecognised character while tokenizing whitespace IMP: %c (ASCII: %d)", imp_c2, imp_c2);
+                }
+            case SPACE:
+                if(tokenize_stack_manip(fptr, &tokens, &tokens_cap, &tokens_count) == EXIT_FAILURE){
+                    return NULL;
                 }
         }
     }
