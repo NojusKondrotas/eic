09b3a99b1dd1f268afdf7c629976394775cc8970
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-06-02 22:01:16 +0300

    Add signed size_t function variants

--- src/runtime/stack.c
+++ src/runtime/stack.c
@@ -26,3 +26,28 @@ int size_t_ensure_cap(size_t **tokens, size_t *tokens_cap, size_t *tokens_count)
 
     return EXIT_SUCCESS;
 }
+
+int ssize_t_push_token(ssize_t **tokens, size_t *tokens_cap, size_t *tokens_count, int token){
+    if(*tokens_cap == *tokens_count){
+        if(size_t_ensure_cap(tokens, tokens_cap, tokens_count) == EXIT_FAILURE)
+            return EXIT_FAILURE;
+    }
+    (*tokens)[(*tokens_count)++] = token;
+    
+    return EXIT_SUCCESS;
+}
+
+int ssize_t_ensure_cap(ssize_t **tokens, size_t *tokens_cap, size_t *tokens_count){
+    if(*tokens_cap == *tokens_count){
+        (*tokens_cap) *= 2;
+        int *tmp = realloc(*tokens, *tokens_cap * sizeof(int));
+        if(!tmp){
+            fprintf(stderr, "Failure allocating memory\n");
+            return EXIT_FAILURE;
+        }
+
+        *tokens = tmp;
+    }
+
+    return EXIT_SUCCESS;
+}
--- src/runtime/stack.h
+++ src/runtime/stack.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
+typedef long ssize_t;
+
 int int_push_token(int **, size_t *, size_t *, int token);
 
 int int_ensure_cap(int **, size_t *, size_t *);
